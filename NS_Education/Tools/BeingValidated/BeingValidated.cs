4698f7a66b8573a1f7aef60ab63b190a7bcf39a1
KevinHuang <xiangwei.326@gmail.com>
2023-04-10 09:52:06 +0800

    Revert "Delete: 刪除不需要的 classes"
    This reverts commit fb1513cd5633c7777f4c821c42ecfdf487453dfc.

---
+++ NS_Education/Tools/BeingValidated/BeingValidated.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NS_Education.Tools.BeingValidated
+{
+    public class BeingValidated<T> : IBeingValidated<T>
+    {
+        private readonly T _target;
+        private bool _isInvalid;
+        private readonly bool _lazy;
+
+        public BeingValidated(T target, bool lazy = false)
+        {
+            _target = target;
+            _lazy = lazy;
+        }
+
+        public BeingValidated<T> Validate(Func<T, bool> validation)
+        {
+            if (_lazy && _isInvalid)
+                return this;
+            
+            if (!validation.Invoke(_target))
+                _isInvalid = true;
+
+            return this;
+        }
+
+        public bool Result()
+        {
+            return _isInvalid;
+        }
+    }
+}

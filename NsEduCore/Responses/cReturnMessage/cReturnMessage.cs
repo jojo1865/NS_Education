538073f9f28d9def88e5806eece4566972f06537
KevinHuang <xiangwei.326@gmail.com>
2023-04-12 09:09:03 +0800

    Revert "Add: Login"
    This reverts commit 3d39b4048efaec7a9403e73096be84f173312dd8.

--- NsEduCore/Responses/cReturnMessage/cReturnMessage.cs
+++
@@ -1,8 +0,0 @@
-namespace NsEduCore.Responses.cReturnMessage
-{
-    public class cReturnMessage
-    {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-    }
-}

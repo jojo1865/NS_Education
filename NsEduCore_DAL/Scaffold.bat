5c7a23d99b736ba2874dbfa8f39b47cbb778ea49
KevinHuang <xiangwei.326@gmail.com>
2023-04-12 09:09:07 +0800

    Revert "initial commit"
    This reverts commit 0a7d02954cd0585d87be2d635e957bfe45e87b81.

--- NsEduCore_DAL/Scaffold.bat
+++
@@ -1,1 +0,0 @@
-dotnet ef dbcontext scaffold "Data Source=LAPTOP-RUU8RF7Q\NS_EDUCATION;Initial Catalog=ns;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer --use-database-names --no-pluralize --context-dir Models\Data --output-dir Models --context NsDataContext 

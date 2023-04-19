893e29f99d4720d4272f1f6126ba8a70659b78b0
KevinHuang <xiangwei.326@gmail.com>
2023-04-19 14:08:15 +0800

    Refactor: 補足註解

--- NS_Education/Controllers/BaseClass/BaseController.cs
+++ NS_Education/Controllers/BaseClass/BaseController.cs
@@ -278,7 +278,7 @@ namespace NS_Education.Controllers.BaseClass
                 return GetResponseJson();
             }
 
-            // 3. 實際修改並更新。
+            // 3. 實際更新起用狀態與更新者資訊，並寫入 DB。
             try
             {
                 SetProperty(t, ActiveFlag, activeFlagValue);
@@ -350,7 +350,7 @@ namespace NS_Education.Controllers.BaseClass
                 return GetResponseJson();
             }
 
-            // 3. 更新刪除狀態。
+            // 3. 更新刪除狀態與更新者資訊，並存入 DB。
             try
             {
                 SetProperty(t, DeleteFlag, true);
@@ -386,24 +386,58 @@ namespace NS_Education.Controllers.BaseClass
 
         #region Submit
 
+        private const string SubmitAddValidateFailed = "欲新增資料的輸入格式不符！";
+        private const string SubmitEditValidateFailed = "欲更新資料的輸入格式不符！";
         private const string SubmitEditNotFound = "查無欲更新的資料！";
 
+        /// <summary>
+        /// 新增或更新一筆資料。
+        /// </summary>
+        /// <param name="input">輸入資料</param>
+        /// <returns>
+        /// 成功時：通用訊息回傳格式。<br/>
+        /// 輸入驗證失敗時：包含錯誤訊息的通用訊息回傳格式。<br/>
+        /// 新增，但 DB 連線異常時：包含錯誤訊息的通用訊息回傳格式。<br/>
+        /// 修改，但查無資料，或 DB 連線異常時：包含錯誤訊息的通用訊息回傳格式。<br/>
+        /// 其他錯誤時：拋錯。
+        /// </returns>
         [HttpGet]
         [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.AddOrEdit, null, DefaultAddOrEditKeyFieldName)]
         public virtual async Task<string> Submit(TSubmitRequest input)
         {
-            if (SubmitIsAdd(input) && await SubmitAddValidateInput(input))
+            // 1. 依據實作內容判定此次 Submit 為新增還是更新。
+            // 2. 依據新增或更新模式，進行個別的輸入驗證。
+            // |- a. 驗證通過時：執行邏輯。
+            // +- b. 驗證失敗，且無錯誤訊息時：自動加上預設錯誤訊息。
+            
+            if (SubmitIsAdd(input))
             {
-                await _SubmitAdd(input);
+                if (await SubmitAddValidateInput(input))
+                    await _SubmitAdd(input);
+                else if (!HasError())
+                    AddError(SubmitAddValidateFailed);
             }
-            else if (await SubmitEditValidateInput(input))
+            else
             {
-                await _SubmitEdit(input);
+                if (await SubmitEditValidateInput(input))
+                    await _SubmitEdit(input);
+                else if (!HasError())
+                    AddError(SubmitEditValidateFailed);
             }
+            
+            // 3. 回傳通用回傳訊息格式。
 
             return GetResponseJson();
         }
 
+        /// <summary>
+        /// 新增或更新一筆資料時，判定此次要求是否為新增。
+        /// </summary>
+        /// <param name="input">輸入資料</param>
+        /// <returns>
+        /// true：為新增。<br/>
+        /// false：不是新增（視為更新）。
+        /// </returns>
         protected abstract bool SubmitIsAdd(TSubmitRequest input);
 
         #region Submit - Add
@@ -426,7 +460,22 @@ namespace NS_Education.Controllers.BaseClass
             }
         }
 
+        /// <summary>
+        /// 新增一筆資料時，驗證輸入格式。
+        /// </summary>
+        /// <param name="input">輸入</param>
+        /// <returns>
+        /// true：驗證通過。<br/>
+        /// false：驗證錯誤。
+        /// </returns>
         protected abstract Task<bool> SubmitAddValidateInput(TSubmitRequest input);
+        
+        /// <summary>
+        /// 新增一筆資料時，依據輸入建立新物件的方法。
+        /// </summary>
+        /// <param name="input">輸入</param>
+        /// <returns>欲新增的物件</returns>
+        /// <remarks>若此方法是藉由預設的 Submit 方法被呼叫時，實作者可以忽略 CreUid、CreDate、UpdUid 及 UpdDate 的設定。</remarks>
         protected abstract TEntity SubmitCreateData(TSubmitRequest input);
 
         #endregion
@@ -459,8 +508,30 @@ namespace NS_Education.Controllers.BaseClass
             }
         }
 
+        /// <summary>
+        /// 更新一筆資料時，驗證輸入格式。
+        /// </summary>
+        /// <param name="input">輸入</param>
+        /// <returns>
+        /// true：驗證通過。<br/>
+        /// false：驗證錯誤。
+        /// </returns>
         protected abstract Task<bool> SubmitEditValidateInput(TSubmitRequest input);
+        
+        /// <summary>
+        /// 更新一筆資料時，依據輸入覆寫資料各欄位的方法。
+        /// </summary>
+        /// <param name="data">DB 資料</param>
+        /// <param name="input">輸入</param>
+        /// <remarks>若此方法是藉由預設的 Submit 方法被呼叫時，實作者在更新時可以忽略 UpdUid 及 UpdDate 的更新。</remarks>
         protected abstract void SubmitEditUpdateDataFields(TEntity data, TSubmitRequest input);
+        
+        /// <summary>
+        /// 更新一筆資料時，找出原資料的查詢。
+        /// </summary>
+        /// <param name="input">輸入</param>
+        /// <returns>查詢</returns>
+        /// <remarks>若此方法是藉由預設的 Submit 方法被呼叫時，實作者在查詢中可以忽略 DeleteFlag 的判定。</remarks>
         protected abstract IQueryable<TEntity> SubmitEditQuery(TSubmitRequest input);
 
         #endregion

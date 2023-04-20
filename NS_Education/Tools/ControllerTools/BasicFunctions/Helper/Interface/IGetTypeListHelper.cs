b2dea47b4786bac4b3520d79e1bce0550f7c2b5a
KevinHuang <xiangwei.326@gmail.com>
2023-04-20 14:11:33 +0800

    Fix: 修正註解

--- NS_Education/Tools/ControllerTools/BasicFunctions/Helper/Interface/IGetTypeListHelper.cs
+++ NS_Education/Tools/ControllerTools/BasicFunctions/Helper/Interface/IGetTypeListHelper.cs
@@ -12,7 +12,6 @@ namespace NS_Education.Tools.ControllerTools.BasicFunctions.Helper.Interface
         /// </summary>
         /// <returns>
         /// 成功時：包含列表的通用訊息回傳格式。<br/>
-        /// 驗證失敗，或找不到資料時：不包含列表的通用訊息回傳格式。<br/>
         /// 意外錯誤時：拋錯。
         /// </returns>
         Task<string> GetTypeList();

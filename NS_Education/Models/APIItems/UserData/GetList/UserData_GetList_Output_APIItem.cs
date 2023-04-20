c3fd51b041df6e046a8b72925ebb35bae34560f9
KevinHuang <xiangwei.326@gmail.com>
2023-04-20 11:13:29 +0800

    Add: 增加沒有 Request 的 GetList 介面

--- NS_Education/Models/APIItems/UserData/GetList/UserData_GetList_Output_APIItem.cs
+++ NS_Education/Models/APIItems/UserData/GetList/UserData_GetList_Output_APIItem.cs
@@ -1,6 +1,6 @@
 namespace NS_Education.Models.APIItems.UserData.GetList
 {
-    public class UserData_GetList_Output_APIItem : BaseResponseForList<UserData_GetList_Output_Row_APIItem>
+    public class UserData_GetList_Output_APIItem : BaseResponseForPagedList<UserData_GetList_Output_Row_APIItem>
     {
     }
 }
--- NS_Education/Tools/ControllerTools/BasicFunctions/Interface/IGetList.cs
+++ NS_Education/Tools/ControllerTools/BasicFunctions/Interface/IGetList.cs
@@ -53,4 +53,40 @@ namespace NS_Education.Tools.ControllerTools.BasicFunctions.Interface
         /// <returns>Response 所需類型的單筆資料</returns>
         Task<TGetListRow> GetListEntityToRow(TEntity entity);
     }
+    
+    /// <summary>
+    /// GetList 功能 API 端點的介面。傳入時單純只執行 GET 而無其他參數，回傳所有掌管資料。
+    /// </summary>
+    /// <typeparam name="TEntity">掌管資料的類型</typeparam>
+    /// <typeparam name="TGetListRow">回傳時，List 中子物件的類型</typeparam>
+    public interface IGetList<TEntity, TGetListRow>
+        where TEntity : class
+        where TGetListRow : class
+    {
+        /// <summary>
+        /// 取得所有資料。
+        /// </summary>
+        /// <returns>
+        /// 成功時：包含列表的通用訊息回傳格式。<br/>
+        /// 驗證失敗，或找不到資料時：不包含列表的通用訊息回傳格式。<br/>
+        /// 意外錯誤時：拋錯。
+        /// </returns>
+        Task<string> GetList();
+        
+        /// <summary>
+        /// 依據取得列表的輸入資料，取得查詢。<br/>
+        /// 實作者可以在這個方法中進行 AddError，回到主方法後就不會實際執行查詢，而是提早回傳。<br/>
+        /// </summary>
+        /// <returns>具備排序的查詢。</returns>
+        /// <remarks>若此方法是藉由 Helper 被呼叫時，實作者在查詢中可以忽略 DeleteFlag 的判定。</remarks>
+        IOrderedQueryable<TEntity> GetListOrderedQuery();
+
+        /// <summary>
+        /// 將取得列表的查詢結果轉換成 Response 所需的子物件類型。。<br/>
+        /// 時作者可以在這個方法中進行 AddError，最後回傳結果仍會包含資料，但會告知前端結果並不成功。（Success = false）
+        /// </summary>
+        /// <param name="entity">單筆查詢結果</param>
+        /// <returns>Response 所需類型的單筆資料</returns>
+        Task<TGetListRow> GetListEntityToRow(TEntity entity);
+    }
 }

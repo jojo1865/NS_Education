cc7143e6a38a44a6e68949da05fb1aa89030889c
KevinHuang <xiangwei.326@gmail.com>
2023-04-24 14:55:14 +0800

    Change: 改修 PayType 的 GetList 和 DeleteItem

--- NS_Education/Controller/Legacy/PayTypeController.cs
+++ NS_Education/Controller/Legacy/PayTypeController.cs
@@ -5,75 +5,93 @@ using System.Web.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NS_Education.Models;
 using NS_Education.Models.APIItems.PayType;
+using NS_Education.Models.APIItems.PayType.GetList;
 using NS_Education.Models.Entities;
+using NS_Education.Tools.BeingValidated;
 using NS_Education.Tools.ControllerTools.BaseClass;
+using NS_Education.Tools.ControllerTools.BasicFunctions.Helper;
+using NS_Education.Tools.ControllerTools.BasicFunctions.Helper.Interface;
+using NS_Education.Tools.ControllerTools.BasicFunctions.Interface;
+using NS_Education.Tools.Extensions;
 using NS_Education.Tools.Filters.JwtAuthFilter;
 using NS_Education.Tools.Filters.JwtAuthFilter.PrivilegeType;
 
 namespace NS_Education.Controller.Legacy
 {
-    public class PayTypeController : PublicClass
+    public class PayTypeController : PublicClass,
+        IGetListPaged<D_PayType, PayType_GetList_Input_APIItem, PayType_GetList_Output_APIItem>,
+        IDeleteItem<D_PayType>
     {
+        #region Initialization
+
+        private IGetListPagedHelper<PayType_GetList_Input_APIItem> _getListPagedHelper;
+        private IDeleteItemHelper _deleteItemHelper;
+
+        public PayTypeController()
+        {
+            _getListPagedHelper = new GetListPagedHelper<PayTypeController, D_PayType, PayType_GetList_Input_APIItem,
+                PayType_GetList_Output_APIItem>(this);
+            _deleteItemHelper = new DeleteItemHelper<PayTypeController, D_PayType>(this);
+        }
+
+        #endregion
+
+        #region GetList
+
         [HttpGet]
         [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.ShowFlag, null, null)]
-        public async Task<string> GetList(string KeyWord = "", int BCID = 0, int NowPage = 1, int CutPage = 10)
+        public async Task<string> GetList(PayType_GetList_Input_APIItem input)
         {
-            var Ns = DC.D_PayType.Where(q => !q.DeleteFlag);
-            if (BCID > 0)
-                Ns = Ns.Where(q => q.BCID == BCID);
-            if (KeyWord != "")
-                Ns = Ns.Where(q => q.Title.Contains(KeyWord) || q.Code.Contains(KeyWord));
-
-            D_PayType_List ListData = new D_PayType_List();
-            ListData.Items = new List<D_PayType_APIItem>();
-            ListData.NowPage = NowPage;
-            ListData.CutPage = CutPage;
-
-            if (NowPage == 0)
-                Ns = Ns.Where(q=>q.ActiveFlag).OrderBy(q => q.Title);
-            else
-                Ns = Ns.OrderBy(q => q.Title).Skip((NowPage - 1) * CutPage).Take(CutPage);
-
-            Ns = Ns.Include(q => q.BC);
-
-            var NsList = await Ns.ToListAsync();
-            ListData.SuccessFlag = NsList.Any();
-            ListData.Message = ListData.SuccessFlag ? "" : "查無資料";
-            ListData.AllItemCt = NsList.Count;
-            ListData.AllPageCt = NowPage == 0 ? 0 : (ListData.AllItemCt % CutPage == 0 ? ListData.AllItemCt / CutPage : (ListData.AllItemCt / CutPage) + 1);
-            
-            foreach (var N in NsList)
-            {
-                ListData.Items.Add(new D_PayType_APIItem
-                {
-                    DPTID = N.DPTID,
-                    BCID = N.BCID,
-                    BC_TitleC = N.BC.TitleC,
-                    BC_TitleE = N.BC.TitleE,
-                    BC_List = null,
-                    Code = N.Code,
-                    Title = N.Title,
-
-                    AccountingNo = N.AccountingNo,
-                    CustormerNo = N.CustormerNo,
-                    InvoiceFlag = N.InvoiceFlag,
-                    DepositFlag = N.DepositFlag,
-                    RestaurantFlag = N.RestaurantFlag,
-                    SimpleCheckoutFlag = N.SimpleCheckoutFlag,
-                    SimpleDepositFlag = N.SimpleDepositFlag,
-
-                    ActiveFlag = N.ActiveFlag,
-                    CreDate = N.CreDate.ToString(DateTimeFormat),
-                    CreUser = await GetUserNameByID(N.CreUID),
-                    CreUID = N.CreUID,
-                    UpdDate = (N.CreDate != N.UpdDate ? N.UpdDate.ToString(DateTimeFormat) : ""),
-                    UpdUser = (N.CreDate != N.UpdDate ? await GetUserNameByID(N.UpdUID) : ""),
-                    UpdUID = (N.CreDate != N.UpdDate ? N.UpdUID : 0)
-                }); ; ;
-            }
-
-            return ChangeJson(ListData);
+            return await _getListPagedHelper.GetPagedList(input);
         }
+
+        public async Task<bool> GetListPagedValidateInput(PayType_GetList_Input_APIItem input)
+        {
+            bool isValid = input.StartValidate()
+                .Validate(i => i.BCID.IsValidIdOrZero(), () => AddError(EmptyNotAllowed("所屬分類 ID")))
+                .IsValid();
+
+            return await Task.FromResult(isValid);
+        }
+
+        public IOrderedQueryable<D_PayType> GetListPagedOrderedQuery(PayType_GetList_Input_APIItem input)
+        {
+            var query = DC.D_PayType
+                .Include(pt => pt.BC)
+                .AsQueryable();
+
+            if (!input.Keyword.IsNullOrWhiteSpace())
+                query = query.Where(pt => pt.Title.Contains(input.Keyword) || pt.Code.Contains(input.Keyword));
+
+            if (input.BCID > 0)
+                query = query.Where(pt => pt.BCID == input.BCID);
+
+            return query.OrderBy(pt => pt.DPTID);
+        }
+
+        public async Task<PayType_GetList_Output_APIItem> GetListPagedEntityToRow(D_PayType entity)
+        {
+            return await Task.FromResult(new PayType_GetList_Output_APIItem
+            {
+                DPTID = entity.DPTID,
+                BCID = entity.BCID,
+                BC_TitleC = entity.BC.TitleC ?? "",
+                BC_TitleE = entity.BC.TitleE ?? "",
+                Code = entity.Code ?? "",
+                Title = entity.Title ?? "",
+                AccountingNo = entity.AccountingNo,
+                CustomerNo = entity.CustormerNo,
+                InvoiceFlag = entity.InvoiceFlag,
+                DepositFlag = entity.DepositFlag,
+                RestaurantFlag = entity.RestaurantFlag,
+                SimpleCheckoutFlag = entity.SimpleCheckoutFlag,
+                SimpleDepositFlag = entity.DepositFlag
+            });
+        }
+
+        #endregion
+
+        #region GetInfoById
 
         [HttpGet]
         [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.ShowFlag, null, null)]
@@ -118,6 +136,10 @@ namespace NS_Education.Controller.Legacy
             return ChangeJson(Item);
         }
 
+        #endregion
+
+        #region ChangeActive
+
         [HttpGet]
         [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.EditFlag, null, null)]
         public async Task<string> ChangeActive(int ID, bool ActiveFlag)
@@ -137,24 +159,25 @@ namespace NS_Education.Controller.Legacy
             return ChangeJson(GetMsgClass(Error));
         }
 
+        #endregion
+
+        #region DeleteItem
+
         [HttpGet]
         [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.DeleteFlag, null, null)]
-        public async Task<string> DeleteItem(int ID)
+        public async Task<string> DeleteItem(int id, bool? deleteFlag)
         {
-            Error = "";
-            var N_ = await DC.D_PayType.FirstOrDefaultAsync(q => q.DPTID == ID);
-            if (N_ != null)
-            {
-                N_.DeleteFlag = true;
-                N_.UpdDate = DT;
-                N_.UpdUID = GetUid();
-                await DC.SaveChangesAsync();
-            }
-            else
-                Error += "查無資料,無法更新;";
-
-            return ChangeJson(GetMsgClass(Error));
+            return await _deleteItemHelper.DeleteItem(id, deleteFlag);
         }
+
+        public IQueryable<D_PayType> DeleteItemQuery(int id)
+        {
+            return DC.D_PayType.Where(pt => pt.DPTID == id);
+        }
+
+        #endregion
+
+        #region Submit
 
         [HttpPost]
         [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.AddOrEdit, null, nameof(D_PayType.DPTID))]
@@ -204,7 +227,10 @@ namespace NS_Education.Controller.Legacy
                     await DC.SaveChangesAsync();
                 }
             }
+
             return ChangeJson(GetMsgClass(Error));
         }
+
+        #endregion
     }
 }
---
+++ NS_Education/Models/APIItems/PayType/GetList/PayType_GetList_Output_APIItem.cs
@@ -0,0 +1,23 @@
+using NS_Education.Models.Entities;
+
+namespace NS_Education.Models.APIItems.PayType.GetList
+{
+    public class PayType_GetList_Output_APIItem : BaseResponseWithCreUpd<D_PayType>
+    {
+        public int DPTID { get; set; }
+        public int BCID { get; set; }
+        public string BC_TitleC { get; set; }
+        public string BC_TitleE { get; set; }
+
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public string AccountingNo { get; set; }
+        public string CustomerNo { get; set; }
+
+        public bool InvoiceFlag { get; set; }
+        public bool DepositFlag { get; set; }
+        public bool RestaurantFlag { get; set; }
+        public bool SimpleCheckoutFlag { get; set; }
+        public bool SimpleDepositFlag { get; set; }
+    }
+}

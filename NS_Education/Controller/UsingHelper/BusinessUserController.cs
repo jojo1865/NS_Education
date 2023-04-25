1238b07197ea2b04fa942786b8968b9a5a8d944a
KevinHuang <xiangwei.326@gmail.com>
2023-04-25 16:18:27 +0800

    Add: 完成 BusinessUser.GetList

---
+++ NS_Education/Controller/UsingHelper/BusinessUserController.cs
@@ -0,0 +1,89 @@
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NS_Education.Models.APIItems.BusinessUser.GetList;
+using NS_Education.Models.Entities;
+using NS_Education.Tools.ControllerTools.BaseClass;
+using NS_Education.Tools.ControllerTools.BasicFunctions.Helper;
+using NS_Education.Tools.ControllerTools.BasicFunctions.Helper.Interface;
+using NS_Education.Tools.ControllerTools.BasicFunctions.Interface;
+using NS_Education.Tools.Extensions;
+using NS_Education.Tools.Filters.JwtAuthFilter;
+using NS_Education.Tools.Filters.JwtAuthFilter.PrivilegeType;
+
+namespace NS_Education.Controller.UsingHelper
+{
+    public class BusinessUserController : PublicClass,
+        IGetListPaged<BusinessUser, BusinessUser_GetList_Input_APIItem, BusinessUser_GetList_Output_Row_APIItem>
+    {
+        #region Initialization
+
+        private readonly IGetListPagedHelper<BusinessUser_GetList_Input_APIItem> _getListPagedHelper;
+
+        public BusinessUserController()
+        {
+            _getListPagedHelper =
+                new GetListPagedHelper<BusinessUserController, BusinessUser, BusinessUser_GetList_Input_APIItem,
+                    BusinessUser_GetList_Output_Row_APIItem>(this);
+        }
+
+        #endregion
+        
+        #region GetList
+        [HttpGet]
+        [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.ShowFlag, null, null)]
+        public async Task<string> GetList(BusinessUser_GetList_Input_APIItem input)
+        {
+            return await _getListPagedHelper.GetPagedList(input);
+        }
+
+        public async Task<bool> GetListPagedValidateInput(BusinessUser_GetList_Input_APIItem input)
+        {
+            // 此功能不需驗證輸入
+            return await Task.FromResult(true);
+        }
+
+        public IOrderedQueryable<BusinessUser> GetListPagedOrderedQuery(BusinessUser_GetList_Input_APIItem input)
+        {
+            var query = DC.BusinessUser
+                .Include(bu => bu.M_Customer_BusinessUser)
+                .ThenInclude(cbu => cbu.C)
+                .AsQueryable();
+
+            if (!input.Keyword.IsNullOrWhiteSpace())
+                query = query.Where(bu => bu.Name.Contains(input.Keyword) || bu.Code.Contains(input.Keyword));
+
+            if (input.MKType.IsInBetween(0, 1))
+                query = query.Where(bu => bu.MKsalesFlag == (input.MKType == 1));
+            
+            if (input.OPType.IsInBetween(0, 1))
+                query = query.Where(bu => bu.OPsalesFlag == (input.OPType == 1));
+
+            return query.OrderBy(bu => bu.Code)
+                .ThenBy(bu => bu.Name)
+                .ThenBy(bu => bu.BUID);
+        }
+
+        public async Task<BusinessUser_GetList_Output_Row_APIItem> GetListPagedEntityToRow(BusinessUser entity)
+        {
+            return await Task.FromResult(new BusinessUser_GetList_Output_Row_APIItem
+            {
+                BUID = entity.BUID,
+                Code = entity.Code ?? "",
+                Name = entity.Name ?? "",
+                Phone = entity.Phone ?? "",
+                MKsalesFlag = entity.MKsalesFlag,
+                OPsalesFlag = entity.OPsalesFlag,
+                Items = entity.M_Customer_BusinessUser.Select(cbu => new BusinessUser_GetList_Output_Customer_APIItem
+                {
+                    CID = cbu.CID,
+                    Code = cbu.C?.Code ?? "",
+                    TitleC = cbu.C?.TitleC ?? "",
+                    TitleE = cbu.C?.TitleE ?? ""
+                }).ToList()
+            });
+        }
+        #endregion
+    }
+}
---
+++ NS_Education/Models/APIItems/BusinessUser/GetList/BusinessUser_GetList_Input_APIItem.cs
@@ -0,0 +1,9 @@
+namespace NS_Education.Models.APIItems.BusinessUser.GetList
+{
+    public class BusinessUser_GetList_Input_APIItem : BaseRequestForList
+    {
+        public string Keyword { get; set; }
+        public int MKType { get; set; } = -1;
+        public int OPType { get; set; } = -1;
+    }
+}

97428de206ebd7011a7b2706b2b6567bdb6b14f4
KevinHuang <xiangwei.326@gmail.com>
2023-04-25 16:37:33 +0800

    Add: 完成 BusinessUser 的 GetInfoById

--- NS_Education/Controller/UsingHelper/BusinessUserController.cs
+++ NS_Education/Controller/UsingHelper/BusinessUserController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NS_Education.Models.APIItems.BusinessUser.GetInfoById;
 using NS_Education.Models.APIItems.BusinessUser.GetList;
 using NS_Education.Models.Entities;
 using NS_Education.Tools.ControllerTools.BaseClass;
@@ -15,22 +16,29 @@ using NS_Education.Tools.Filters.JwtAuthFilter.PrivilegeType;
 namespace NS_Education.Controller.UsingHelper
 {
     public class BusinessUserController : PublicClass,
-        IGetListPaged<BusinessUser, BusinessUser_GetList_Input_APIItem, BusinessUser_GetList_Output_Row_APIItem>
+        IGetListPaged<BusinessUser, BusinessUser_GetList_Input_APIItem, BusinessUser_GetList_Output_Row_APIItem>,
+        IGetInfoById<BusinessUser, BusinessUser_GetInfoById_Output_APIItem>
     {
         #region Initialization
 
         private readonly IGetListPagedHelper<BusinessUser_GetList_Input_APIItem> _getListPagedHelper;
+        private readonly IGetInfoByIdHelper _getInfoByIdHelper;
 
         public BusinessUserController()
         {
             _getListPagedHelper =
                 new GetListPagedHelper<BusinessUserController, BusinessUser, BusinessUser_GetList_Input_APIItem,
                     BusinessUser_GetList_Output_Row_APIItem>(this);
+
+            _getInfoByIdHelper =
+                new GetInfoByIdHelper<BusinessUserController, BusinessUser, BusinessUser_GetInfoById_Output_APIItem>(
+                    this);
         }
 
         #endregion
-        
+
         #region GetList
+
         [HttpGet]
         [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.ShowFlag, null, null)]
         public async Task<string> GetList(BusinessUser_GetList_Input_APIItem input)
@@ -56,7 +64,7 @@ namespace NS_Education.Controller.UsingHelper
 
             if (input.MKType.IsInBetween(0, 1))
                 query = query.Where(bu => bu.MKsalesFlag == (input.MKType == 1));
-            
+
             if (input.OPType.IsInBetween(0, 1))
                 query = query.Where(bu => bu.OPsalesFlag == (input.OPType == 1));
 
@@ -84,6 +92,41 @@ namespace NS_Education.Controller.UsingHelper
                 }).ToList()
             });
         }
+
+        #endregion
+
+        #region GetInfoById
+
+        [HttpGet]
+        [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.ShowFlag, null, null)]
+        public async Task<string> GetInfoById(int id)
+        {
+            return await _getInfoByIdHelper.GetInfoById(id);
+        }
+
+        public IQueryable<BusinessUser> GetInfoByIdQuery(int id)
+        {
+            return DC.BusinessUser
+                .Include(bu => bu.M_Customer_BusinessUser)
+                .Where(bu => bu.BUID == id);
+        }
+
+        public async Task<BusinessUser_GetInfoById_Output_APIItem> GetInfoByIdConvertEntityToResponse(
+            BusinessUser entity)
+        {
+            return await Task.FromResult(new BusinessUser_GetInfoById_Output_APIItem
+                {
+                    BUID = entity.BUID,
+                    Code = entity.Code ?? "",
+                    Name = entity.Name ?? "",
+                    Phone = entity.Phone ?? "",
+                    MKsalesFlag = entity.MKsalesFlag,
+                    OPsalesFlag = entity.OPsalesFlag,
+                    CustomerCt = entity.M_Customer_BusinessUser.Count(cbu => cbu.ActiveFlag && !cbu.DeleteFlag)
+                }
+            );
+        }
+
         #endregion
     }
 }

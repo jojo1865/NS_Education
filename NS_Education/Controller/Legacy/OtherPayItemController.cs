916fe5f50d2e032184eb401fdea2d7403339294f
KevinHuang <xiangwei.326@gmail.com>
2023-04-25 11:02:39 +0800

    Change: 改修 OtherPayItem 的 GetList, DeleteItem, Submit

--- NS_Education/Controller/Legacy/OtherPayItemController.cs
+++ NS_Education/Controller/Legacy/OtherPayItemController.cs
@@ -1,67 +1,92 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NS_Education.Models.APIItems.OtherPayItem;
+using NS_Education.Models.APIItems.OtherPayItem.GetList;
+using NS_Education.Models.APIItems.OtherPayItem.Submit;
 using NS_Education.Models.Entities;
+using NS_Education.Tools.BeingValidated;
 using NS_Education.Tools.ControllerTools.BaseClass;
+using NS_Education.Tools.ControllerTools.BasicFunctions.Helper;
+using NS_Education.Tools.ControllerTools.BasicFunctions.Helper.Interface;
+using NS_Education.Tools.ControllerTools.BasicFunctions.Interface;
+using NS_Education.Tools.Extensions;
+using NS_Education.Tools.Filters.JwtAuthFilter;
+using NS_Education.Tools.Filters.JwtAuthFilter.PrivilegeType;
 
 namespace NS_Education.Controller.Legacy
 {
-    public class OtherPayItemController : PublicClass
+    public class OtherPayItemController : PublicClass,
+        IGetListPaged<D_OtherPayItem, OtherPayItem_GetList_Input_APIItem, OtherPayItem_GetList_Output_Row_APIItem>,
+        IDeleteItem<D_OtherPayItem>,
+        ISubmit<D_OtherPayItem, OtherPayItem_Submit_Input_APIItem>
     {
-        [HttpGet]
-        public async Task<string> GetList(string KeyWord = "", int NowPage = 1, int CutPage = 10)
+        #region Initialization
+
+        private readonly IGetListPagedHelper<OtherPayItem_GetList_Input_APIItem> _getListPagedHelper;
+        private readonly IDeleteItemHelper _deleteItemHelper;
+        private readonly ISubmitHelper<OtherPayItem_Submit_Input_APIItem> _submitHelper;
+
+        public OtherPayItemController()
         {
-            var Ns = DC.D_OtherPayItem.Where(q => !q.DeleteFlag);
-            
-            if (KeyWord != "")
-                Ns = Ns.Where(q => q.Title.Contains(KeyWord) || q.Code.Contains(KeyWord));
+            _getListPagedHelper =
+                new GetListPagedHelper<OtherPayItemController, D_OtherPayItem, OtherPayItem_GetList_Input_APIItem,
+                    OtherPayItem_GetList_Output_Row_APIItem>(this);
 
-            D_OtherPayItem_List ListData = new D_OtherPayItem_List();
-            ListData.Items = new List<D_OtherPayItem_APIItem>();
-            ListData.NowPage = NowPage;
-            ListData.CutPage = CutPage;
-            
-            if (NowPage == 0)
-                Ns = Ns.Where(q => q.ActiveFlag).OrderBy(q => q.Title);
-            else
-                Ns = Ns.OrderBy(q => q.Title).Skip((NowPage - 1) * CutPage).Take(CutPage);
-            
-            var NsList = await Ns.ToListAsync();
-            ListData.SuccessFlag = NsList.Any();
-            ListData.Message = ListData.SuccessFlag ? "" : "查無資料";
-            ListData.AllItemCt = NsList.Count;
-            ListData.AllPageCt = NowPage == 0 ? 0 : (ListData.AllItemCt % CutPage == 0 ? ListData.AllItemCt / CutPage : (ListData.AllItemCt / CutPage) + 1);
-
-            foreach (var N in Ns)
-            {
-                ListData.Items.Add(new D_OtherPayItem_APIItem
-                {
-                    DOPIID = N.DOPIID,
-                    Code = N.Code,
-                    Title = N.Title,
-
-                    Ct = N.Ct,
-                    UnitPrice = N.UnitPrice,
-                    InPrice = N.InPrice,
-                    OutPrice = N.OutPrice,
-
-                    ActiveFlag = N.ActiveFlag,
-                    CreDate = N.CreDate.ToString(DateTimeFormat),
-                    CreUser = await GetUserNameByID(N.CreUID),
-                    CreUID = N.CreUID,
-                    UpdDate = (N.CreDate != N.UpdDate ? N.UpdDate.ToString(DateTimeFormat) : ""),
-                    UpdUser = (N.CreDate != N.UpdDate ? await GetUserNameByID(N.UpdUID) : ""),
-                    UpdUID = (N.CreDate != N.UpdDate ? N.UpdUID : 0)
-                });
-            }
-
-            return ChangeJson(ListData);
+            _deleteItemHelper = new DeleteItemHelper<OtherPayItemController, D_OtherPayItem>(this);
+            _submitHelper =
+                new SubmitHelper<OtherPayItemController, D_OtherPayItem, OtherPayItem_Submit_Input_APIItem>(this);
         }
 
+        #endregion
+
+        #region GetList
+
         [HttpGet]
+        [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.ShowFlag, null, null)]
+        public async Task<string> GetList(OtherPayItem_GetList_Input_APIItem input)
+        {
+            return await _getListPagedHelper.GetPagedList(input);
+        }
+
+        public async Task<bool> GetListPagedValidateInput(OtherPayItem_GetList_Input_APIItem input)
+        {
+            // 此功能無須驗證輸入
+            return true;
+        }
+
+        public IOrderedQueryable<D_OtherPayItem> GetListPagedOrderedQuery(OtherPayItem_GetList_Input_APIItem input)
+        {
+            var query = DC.D_OtherPayItem.AsQueryable();
+
+            if (!input.Keyword.IsNullOrWhiteSpace())
+                query = query.Where(opi => opi.Title.Contains(input.Keyword) || opi.Code.Contains(input.Keyword));
+
+            return query.OrderBy(opi => opi.Code)
+                .ThenBy(opi => opi.DOPIID);
+        }
+
+        public async Task<OtherPayItem_GetList_Output_Row_APIItem> GetListPagedEntityToRow(D_OtherPayItem entity)
+        {
+            return await Task.FromResult(new OtherPayItem_GetList_Output_Row_APIItem
+            {
+                DOPIID = entity.DOPIID,
+                Code = entity.Code ?? "",
+                Title = entity.Title ?? "",
+                Ct = entity.Ct,
+                UnitPrice = entity.UnitPrice,
+                InPrice = entity.InPrice,
+                OutPrice = entity.OutPrice
+            });
+        }
+
+        #endregion
+
+        #region GetInfoById
+
+        [HttpGet]
+        [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.ShowFlag, null, null)]
         public async Task<string> GetInfoByID(int ID = 0)
         {
             var N = await DC.D_OtherPayItem.FirstOrDefaultAsync(q => q.DOPIID == ID && !q.DeleteFlag);
@@ -91,110 +116,117 @@ namespace NS_Education.Controller.Legacy
 
             return ChangeJson(Item);
         }
+
+        #endregion
+
+        #region ChangeActive
+
         [HttpGet]
-        public async Task<string> ChangeActive(int ID, bool ActiveFlag, int UID)
+        [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.EditFlag, null, null)]
+        public async Task<string> ChangeActive(int ID, bool ActiveFlag)
         {
             Error = "";
-            if (UID == 0)
-                Error += "缺少更新者ID,無法更新;";
-            else
+            var N_ = await DC.D_OtherPayItem.FirstOrDefaultAsync(q => q.DOPIID == ID && !q.DeleteFlag);
+            if (N_ != null)
             {
-                var N_ = await DC.D_OtherPayItem.FirstOrDefaultAsync(q => q.DOPIID == ID && !q.DeleteFlag);
-                if (N_ != null)
-                {
-                    N_.ActiveFlag = ActiveFlag;
-                    N_.UpdDate = DT;
-                    N_.UpdUID = UID;
-                    await DC.SaveChangesAsync();
-                }
-                else
-                    Error += "查無資料,無法更新;";
+                N_.ActiveFlag = ActiveFlag;
+                N_.UpdDate = DT;
+                N_.UpdUID = GetUid();
+                await DC.SaveChangesAsync();
             }
+            else
+                Error += "查無資料,無法更新;";
 
             return ChangeJson(GetMsgClass(Error));
         }
-        [HttpGet]
-        public async Task<string> DeleteItem(int ID, int UID)
-        {
-            Error = "";
-            if (UID == 0)
-                Error += "缺少更新者ID,無法更新;";
-            else
-            {
-                var N_ = await DC.D_OtherPayItem.FirstOrDefaultAsync(q => q.DOPIID == ID);
-                if (N_ != null)
-                {
-                    N_.DeleteFlag = true;
-                    N_.UpdDate = DT;
-                    N_.UpdUID = UID;
-                    await DC.SaveChangesAsync();
-                }
-                else
-                    Error += "查無資料,無法更新;";
-            }
 
-            return ChangeJson(GetMsgClass(Error));
+        #endregion
+
+        #region DeleteItem
+
+        [HttpGet]
+        [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.DeleteFlag, null, null)]
+        public async Task<string> DeleteItem(int id, bool? deleteFlag)
+        {
+            return await _deleteItemHelper.DeleteItem(id, deleteFlag);
         }
+
+        public IQueryable<D_OtherPayItem> DeleteItemQuery(int id)
+        {
+            return DC.D_OtherPayItem.Where(opi => opi.DOPIID == id);
+        }
+
+        #endregion
+
+        #region Submit
 
         [HttpPost]
-        public async Task<string> Submit(D_OtherPayItem N)
+        [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.AddOrEdit, null,
+            nameof(OtherPayItem_Submit_Input_APIItem.DOPIID))]
+        public async Task<string> Submit(OtherPayItem_Submit_Input_APIItem input)
         {
-            Error = "";
-            if (N.DOPIID == 0)
-            {
-                if (N.CreUID == 0)
-                    Error += "缺少建立者ID,無法更新;";
-               
-                if (N.Title == "")
-                    Error += "名稱必須輸入;";
-                if (N.UnitPrice < 0)
-                    Error += "請輸入成本的數字;";
-                if (N.InPrice < 0)
-                    Error += "請輸入內部價格的數字;";
-                if (N.OutPrice < 0)
-                    Error += "請輸入外部價格的數字;";
-                if (Error == "")
-                {
-                    N.UpdDate = N.CreDate = DT;
-                    N.UpdUID = 0;
-                    await DC.D_OtherPayItem.AddAsync(N);
-                    await DC.SaveChangesAsync();
-                }
-            }
-            else
-            {
-                var N_ = await DC.D_OtherPayItem.FirstOrDefaultAsync(q => q.DOPIID == N.DOPIID && !q.DeleteFlag);
-                if (N.CreUID == 0)
-                    Error += "缺少更新者ID,無法更新;";
-                
-                if (N.Title == "")
-                    Error += "名稱必須輸入;";
-                if (N.UnitPrice < 0)
-                    Error += "請輸入成本的數字;";
-                if (N.InPrice < 0)
-                    Error += "請輸入內部價格的數字;";
-                if (N.OutPrice < 0)
-                    Error += "請輸入外部價格的數字;";
-
-                if (N_ == null)
-                    Error += "查無資料,無法更新";
-                if (Error == "")
-                {
-                    
-                    N_.Code = N.Code;
-                    N_.Title = N.Title;
-                    N_.Ct = N.Ct;
-                    N_.UnitPrice = N.UnitPrice;
-                    N_.InPrice = N.InPrice;
-                    N_.OutPrice = N.OutPrice;
-                    N_.ActiveFlag = N.ActiveFlag;
-                    N_.DeleteFlag = N.DeleteFlag;
-                    N_.UpdUID = N.UpdUID;
-                    N_.UpdDate = DT;
-                    await DC.SaveChangesAsync();
-                }
-            }
-            return ChangeJson(GetMsgClass(Error));
+            return await _submitHelper.Submit(input);
         }
+
+        public bool SubmitIsAdd(OtherPayItem_Submit_Input_APIItem input)
+        {
+            return input.DOPIID == 0;
+        }
+
+        #region Submit - Add
+
+        public async Task<bool> SubmitAddValidateInput(OtherPayItem_Submit_Input_APIItem input)
+        {
+            bool isValid = input.StartValidate()
+                .Validate(i => i.DOPIID == 0, () => AddError(WrongFormat("項目 ID")))
+                .IsValid();
+
+            return await Task.FromResult(isValid);
+        }
+
+        public async Task<D_OtherPayItem> SubmitCreateData(OtherPayItem_Submit_Input_APIItem input)
+        {
+            return await Task.FromResult(new D_OtherPayItem
+            {
+                Code = input.Code,
+                Title = input.Title,
+                Ct = input.Ct,
+                UnitPrice = input.UnitPrice,
+                InPrice = input.InPrice,
+                OutPrice = input.OutPrice
+            });
+        }
+
+        #endregion
+
+        #region Submit - Edit
+
+        public async Task<bool> SubmitEditValidateInput(OtherPayItem_Submit_Input_APIItem input)
+        {
+            bool isValid = input.StartValidate()
+                .Validate(i => i.DOPIID.IsValidId(), () => AddError(EmptyNotAllowed("項目 ID")))
+                .IsValid();
+
+            return await Task.FromResult(isValid);
+        }
+
+        public IQueryable<D_OtherPayItem> SubmitEditQuery(OtherPayItem_Submit_Input_APIItem input)
+        {
+            return DC.D_OtherPayItem.Where(opi => opi.DOPIID == input.DOPIID);
+        }
+
+        public void SubmitEditUpdateDataFields(D_OtherPayItem data, OtherPayItem_Submit_Input_APIItem input)
+        {
+            data.Code = input.Code ?? data.Code;
+            data.Title = input.Title ?? data.Title;
+            data.Ct = input.Ct;
+            data.UnitPrice = input.UnitPrice;
+            data.InPrice = input.InPrice;
+            data.OutPrice = input.OutPrice;
+        }
+
+        #endregion
+
+        #endregion
     }
 }

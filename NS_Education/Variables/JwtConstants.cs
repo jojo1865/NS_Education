e251acbfca8002c2016ac56ba3fead7bf6c2e570
KevinHuang <xiangwei.326@gmail.com>
2023-04-28 16:09:20 +0800

    Change: 調高 JWT 週期

--- NS_Education/Variables/JwtConstants.cs
+++ NS_Education/Variables/JwtConstants.cs
@@ -7,7 +7,7 @@ namespace NS_Education.Variables
         // TODO: key 應該做保護
         public const string Secret = "uNU-<}k>Ui~xz\"UcgtY'$wa37bB2.B>T!uN!+>:XXbxYPH/G-C`g~I1G*nf1lo9";
 
-        public const int ExpireMinutes = 120;
+        public const int ExpireMinutes = 720;
         
         public static string UidClaimType => ClaimTypes.Actor;
 

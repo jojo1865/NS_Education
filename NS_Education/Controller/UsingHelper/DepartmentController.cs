431b55eda88035c634f574d19d9ab6b3c535f22b
KevinHuang <xiangwei.326@gmail.com>
2023-05-02 17:37:25 +0800

    refactor: 調整專案結構

--- NS_Education/Controller/UsingHelper/DepartmentController.cs
+++ NS_Education/Controller/UsingHelper/DepartmentController.cs
@@ -15,7 +15,7 @@ using NS_Education.Tools.Extensions;
 using NS_Education.Tools.Filters.JwtAuthFilter;
 using NS_Education.Tools.Filters.JwtAuthFilter.PrivilegeType;
 
-namespace NS_Education.Controller.Legacy
+namespace NS_Education.Controller.UsingHelper
 {
     public class DepartmentController : PublicClass
         , IGetListPaged<D_Department, Department_GetList_Input_APIItem, Department_GetList_Output_Row_APIItem>

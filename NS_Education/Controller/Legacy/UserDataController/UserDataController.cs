c2b6255de372841817dcecdfa6466534c649aa6c
KevinHuang <xiangwei.326@gmail.com>
2023-05-02 16:47:54 +0800

    Cleanup: 清理沒有用到的物件

--- NS_Education/Controller/Legacy/UserDataController/UserDataController.cs
+++ NS_Education/Controller/Legacy/UserDataController/UserDataController.cs
@@ -33,88 +33,11 @@ namespace NS_Education.Controller.Legacy.UserDataController
         ISubmit<UserData, UserData_Submit_Input_APIItem>,
         IChangeActive<UserData>
     {
-        #region 初始化
-
-        private readonly IGetListPagedHelper<UserData_GetList_Input_APIItem> _getListPagedHelper;
-        private readonly IDeleteItemHelper _deleteItemHelper;
-        private readonly ISubmitHelper<UserData_Submit_Input_APIItem> _submitHelper;
-        private readonly IChangeActiveHelper _changeActiveHelper;
-
-        private readonly IGetInfoByIdHelper _getInfoByIdHelper;
-        public UserDataController()
-        {
-            _getListPagedHelper = new
-                GetListPagedHelper<UserDataController, UserData, UserData_GetList_Input_APIItem,
-                    UserData_GetList_Output_Row_APIItem>(this);
-
-            _deleteItemHelper = new
-                DeleteItemHelper<UserDataController, UserData>(this);
-
-            _submitHelper = new
-                SubmitHelper<UserDataController, UserData, UserData_Submit_Input_APIItem>(this);
-
-            _changeActiveHelper = new
-                ChangeActiveHelper<UserDataController, UserData>(this);
-            _getInfoByIdHelper = new GetInfoByIdHelper<UserDataController, UserData, UserData_GetInfoById_Output_APIItem>(this);
-        }
-
-        #endregion
-
-        #region 錯誤訊息 - 通用
-
-        private const string UpdateUidIncorrect = "未提供欲修改的 UID 或格式不正確！";
-        private const string UserDataNotFound = "這筆使用者不存在或已被刪除！";
-
-        private static string QueryFailed(Exception e) => $"查詢 DB 時出錯，請確認伺服器狀態：{e.Message}！";
-
-        #endregion
-
-        #region 錯誤訊息 - 註冊/更新
-
-        private const string PasswordAlphanumericOnly = "使用者密碼只允許半形英文字母、數字！";
-        private const string SignUpGidIncorrect = "缺少身分 ID 或查無身分資料，無法寫入！";
-        private const string SignUpDdidIncorrect = "缺少部門 ID 或查無部門資料，無法寫入！";
-
-        #endregion
-
-        #region 錯誤訊息 - 登入
-
-        private const string LoginAccountNotFound = "查無此使用者帳號，請重新確認！";
-        private const string LoginPasswordIncorrect = "使用者密碼錯誤！";
-
-        private static string LoginDateUpdateFailed(Exception e)
-            => $"上次登入時間更新失敗，錯誤訊息：{e.Message}！";
-
-        #endregion
-
-        #region 錯誤訊息 - 啟用/停用
-
-        private static string ChangeActiveUpdateFailed(Exception e)
-        {
-            return $"更新 DB 時出錯，請確認伺服器狀態：{e.Message}";
-        }
-
-        #endregion
-
-        #region 錯誤訊息 - 更新密碼
-
-        private static string UpdatePWDbFailed(Exception e) => $"更新密碼時失敗，請確認伺服器狀態：{e.Message}！";
-        private const string UpdatePWPasswordNotEncryptable = "密碼只允許英數字！";
-
-        #endregion
-
-        #region 錯誤訊息 - 查詢
-
-        private const string GetUidIncorrect = "缺少 UID，無法寫入！";
-        private const string GetUserNotFound = "查無此使用者帳號，請重新確認！";
-
-        #endregion
-
         #region SignUp
 
         /// <summary>
-        /// 註冊使用者資料。過程中會驗證使用者輸入，並在回傳時一併報錯。<br/>
-        /// 如果過程驗證都通過，才寫入資料庫。
+        ///     註冊使用者資料。過程中會驗證使用者輸入，並在回傳時一併報錯。<br />
+        ///     如果過程驗證都通過，才寫入資料庫。
         /// </summary>
         /// <param name="input">輸入資料</param>
         [HttpPost]
@@ -129,11 +52,10 @@ namespace NS_Education.Controller.Legacy.UserDataController
             if (!input.GID.IsAboveZero() || !DC.GroupData.Any(g => g.GID == input.GID))
                 AddError(SignUpGidIncorrect);
             if (!input.DDID.IsAboveZero() || !DC.D_Department.Any(d => d.DDID == input.DDID))
-                AddError(SignUpDdidIncorrect);
+                AddError(SignUpDdIdIncorrect);
             if (input.LoginPassword.IsNullOrWhiteSpace())
                 AddError(EmptyNotAllowed("使用者密碼"));
             else
-            {
                 // check and encrypt pw
                 try
                 {
@@ -144,12 +66,10 @@ namespace NS_Education.Controller.Legacy.UserDataController
                     AddError(PasswordAlphanumericOnly);
                     // 這裡不做提早返回，方便一次顯示更多錯誤訊息給使用者
                 }
-            }
 
             // create UserData object, validate the columns along
             // TODO: 引用靜態參數檔，完整驗證使用者欄位
-            int requestUid = GetUid();
-            UserData newUser = new UserData
+            var newUser = new UserData
             {
                 UserName = input.Username.ExecuteIfNullOrWhiteSpace(() => AddError(EmptyNotAllowed("使用者名稱"))),
                 LoginAccount = input.LoginAccount.ExecuteIfNullOrWhiteSpace(() => AddError(EmptyNotAllowed("使用者帳號"))),
@@ -178,10 +98,77 @@ namespace NS_Education.Controller.Legacy.UserDataController
 
         #endregion
 
+        #region 初始化
+
+        private readonly IGetListPagedHelper<UserData_GetList_Input_APIItem> _getListPagedHelper;
+        private readonly IDeleteItemHelper _deleteItemHelper;
+        private readonly ISubmitHelper<UserData_Submit_Input_APIItem> _submitHelper;
+        private readonly IChangeActiveHelper _changeActiveHelper;
+
+        private readonly IGetInfoByIdHelper _getInfoByIdHelper;
+
+        public UserDataController()
+        {
+            _getListPagedHelper = new
+                GetListPagedHelper<UserDataController, UserData, UserData_GetList_Input_APIItem,
+                    UserData_GetList_Output_Row_APIItem>(this);
+
+            _deleteItemHelper = new
+                DeleteItemHelper<UserDataController, UserData>(this);
+
+            _submitHelper = new
+                SubmitHelper<UserDataController, UserData, UserData_Submit_Input_APIItem>(this);
+
+            _changeActiveHelper = new
+                ChangeActiveHelper<UserDataController, UserData>(this);
+            _getInfoByIdHelper =
+                new GetInfoByIdHelper<UserDataController, UserData, UserData_GetInfoById_Output_APIItem>(this);
+        }
+
+        #endregion
+
+        #region 錯誤訊息 - 通用
+
+        private const string UpdateUidIncorrect = "未提供欲修改的 UID 或格式不正確！";
+        private const string UserDataNotFound = "這筆使用者不存在或已被刪除！";
+
+        #endregion
+
+        #region 錯誤訊息 - 註冊/更新
+
+        private const string PasswordAlphanumericOnly = "使用者密碼只允許半形英文字母、數字！";
+        private const string SignUpGidIncorrect = "缺少身分 ID 或查無身分資料，無法寫入！";
+        private const string SignUpDdIdIncorrect = "缺少部門 ID 或查無部門資料，無法寫入！";
+
+        #endregion
+
+        #region 錯誤訊息 - 登入
+
+        private const string LoginAccountNotFound = "查無此使用者帳號，請重新確認！";
+        private const string LoginPasswordIncorrect = "使用者密碼錯誤！";
+
+        private static string LoginDateUpdateFailed(Exception e)
+        {
+            return $"上次登入時間更新失敗，錯誤訊息：{e.Message}！";
+        }
+
+        #endregion
+
+        #region 錯誤訊息 - 更新密碼
+
+        private static string UpdatePWDbFailed(Exception e)
+        {
+            return $"更新密碼時失敗，請確認伺服器狀態：{e.Message}！";
+        }
+
+        private const string UpdatePWPasswordNotEncryptable = "密碼只允許英數字！";
+
+        #endregion
+        
         #region Login
 
         /// <summary>
-        /// 驗證使用者登入，無誤則會回傳使用者的 Username 和 JWT Token。
+        ///     驗證使用者登入，無誤則會回傳使用者的 Username 和 JWT Token。
         /// </summary>
         /// <param name="input">輸入資料</param>
         /// <returns>UserData_Login_Output_APIItem</returns>
@@ -191,7 +178,7 @@ namespace NS_Education.Controller.Legacy.UserDataController
             InitializeResponse();
 
             // 驗證
-            UserData queried = !input.LoginAccount.IsNullOrWhiteSpace()
+            var queried = !input.LoginAccount.IsNullOrWhiteSpace()
                 ? await DC.UserData
                     .Include(u => u.M_Group_User)
                     .FirstOrDefaultAsync(u => u.LoginAccount == input.LoginAccount)
@@ -219,14 +206,14 @@ namespace NS_Education.Controller.Legacy.UserDataController
                 // queried 已經在上面驗證為非 null。目前專案使用 C# 版本不支援 !，所以以此代替。 
                 // ReSharper disable once PossibleNullReferenceException
                 new Claim(JwtConstants.UidClaimType, queried.UID.ToString()),
-                new Claim(ClaimTypes.Role, AuthorizeTypeSingletonFactory.User.GetRoleValue()),
+                new Claim(ClaimTypes.Role, AuthorizeTypeSingletonFactory.User.GetRoleValue())
             };
 
             // 特殊規格：如果擁有特殊 GID 的權限，則認識為管理員
             if (queried.M_Group_User.Any(groupUser => groupUser.GID == JwtConstants.AdminGid))
                 claims.Add(new Claim(ClaimTypes.Role, AuthorizeTypeSingletonFactory.Admin.GetRoleValue()));
 
-            UserData_Login_Output_APIItem output = new UserData_Login_Output_APIItem
+            var output = new UserData_Login_Output_APIItem
             {
                 UID = queried.UID,
                 Username = queried.UserName,
@@ -237,12 +224,12 @@ namespace NS_Education.Controller.Legacy.UserDataController
         }
 
         /// <summary>
-        /// 更新使用者的登入日期時間。
+        ///     更新使用者的登入日期時間。
         /// </summary>
         /// <param name="user">使用者資料</param>
         /// <returns>
-        /// true：更新成功。<br/>
-        /// false：更新失敗。
+        ///     true：更新成功。<br />
+        ///     false：更新失敗。
         /// </returns>
         private async Task<bool> UpdateUserLoginDate(UserData user)
         {
@@ -267,9 +254,9 @@ namespace NS_Education.Controller.Legacy.UserDataController
         #region Submit
 
         /// <summary>
-        /// 更新使用者資料。<br/>
-        /// 如果 LoginPassword 有輸入且與資料庫不同時，則會多驗證 OriginalPassword。<br/>
-        /// Note 欄位無論有無輸入都會更新，因此呼叫者輸入時需要自行維持 Note 的內容。
+        ///     更新使用者資料。<br />
+        ///     如果 LoginPassword 有輸入且與資料庫不同時，則會多驗證 OriginalPassword。<br />
+        ///     Note 欄位無論有無輸入都會更新，因此呼叫者輸入時需要自行維持 Note 的內容。
         /// </summary>
         /// <param name="input">輸入資料</param>
         [HttpPost]
@@ -324,11 +311,11 @@ namespace NS_Education.Controller.Legacy.UserDataController
 
         public void SubmitEditUpdateDataFields(UserData data, UserData_Submit_Input_APIItem input)
         {
-                        // 只在欄位有輸入任何資料時，才更新對應欄位
-                        data.UserName = input.Username.IsNullOrWhiteSpace() ? data.UserName : input.Username;
-                        data.LoginAccount =
+            // 只在欄位有輸入任何資料時，才更新對應欄位
+            data.UserName = input.Username.IsNullOrWhiteSpace() ? data.UserName : input.Username;
+            data.LoginAccount =
                 input.LoginAccount.IsNullOrWhiteSpace() ? data.LoginAccount : input.LoginAccount;
-                        data.LoginPassword =
+            data.LoginPassword =
                 input.LoginPassword.IsNullOrWhiteSpace()
                     ? data.LoginPassword
                     : EncryptPassword(input.LoginPassword);
@@ -337,13 +324,13 @@ namespace NS_Education.Controller.Legacy.UserDataController
             data.Note = input.Note;
 
             data.DDID = input.DDID;
-            
+
             // 如果是管理員，才允許繼續更新後續的欄位
             if (!FilterStaticTools.HasRoleInRequest(HttpContext.Request, AuthorizeBy.Admin)) return;
-            
+
             // 只在是管理員時才允許修改啟用狀態
             data.ActiveFlag = input.ActiveFlag;
-            
+
             // 資料庫建模是 User 一對多 Group，但現在看到的 Wireframe 似乎規劃為每位使用者僅一個權限組
             // 所以在這裡
             // 1. 在 M_Group_User 中查詢此使用者有幾筆權限，如果有多筆就先清空至唯一一筆。
@@ -356,7 +343,7 @@ namespace NS_Education.Controller.Legacy.UserDataController
             if (!groupUsers.Any())
             {
                 // 新增一筆權限資料並返回
-                M_Group_User groupUser = new M_Group_User
+                var groupUser = new M_Group_User
                 {
                     GID = input.GID,
                     UID = data.UID
@@ -371,23 +358,22 @@ namespace NS_Education.Controller.Legacy.UserDataController
                 DC.M_Group_User.RemoveRange(groupUsers.Skip(1));
 
             // 更新權限資料
-            M_Group_User newGroupUser = groupUsers.First();
+            var newGroupUser = groupUsers.First();
             newGroupUser.GID = input.GID;
         }
 
         #endregion
-
 
         #endregion
 
         #region 密碼驗證
 
         /// <summary>
-        /// 針對使用者密碼進行加密。<br/>
-        /// 當使用者密碼為空白、空格、null，或包含非英數字時，回傳 (false, null)。
+        ///     針對使用者密碼進行加密。<br />
+        ///     當使用者密碼為空白、空格、null，或包含非英數字時，回傳 (false, null)。
         /// </summary>
         /// <param name="password">使用者密碼</param>
-        /// <returns>result: 加密結果<br/>encryptedPassword: 加密後的字串</returns>
+        /// <returns>result: 加密結果<br />encryptedPassword: 加密後的字串</returns>
         /// <exception cref="ValidationException">使用者密碼包含半形英數字以外的字元時，回傳此錯誤</exception>
         private static string EncryptPassword(string password)
         {
@@ -405,12 +391,13 @@ namespace NS_Education.Controller.Legacy.UserDataController
         }
 
         /// <summary>
-        /// 驗證輸入密碼是否與指定的加密字串相符。
+        ///     驗證輸入密碼是否與指定的加密字串相符。
         /// </summary>
         /// <param name="input">原始輸入的密碼</param>
         /// <param name="data">資料庫的已加密密碼</param>
-        /// <returns>true：相符<br/>
-        /// false：不相符，或密碼輸入包含半形英數字以外的字元
+        /// <returns>
+        ///     true：相符<br />
+        ///     false：不相符，或密碼輸入包含半形英數字以外的字元
         /// </returns>
         private static bool ValidatePassword(string input, string data)
         {
@@ -464,9 +451,11 @@ namespace NS_Education.Controller.Legacy.UserDataController
         #region UpdatePW
 
         /// <summary>
-        /// 更新使用者密碼。
+        ///     更新使用者密碼。
         /// </summary>
-        /// <param name="input"><see cref="UserData_UpdatePW_Input_APIItem"/></param>
+        /// <param name="input">
+        ///     <see cref="UserData_UpdatePW_Input_APIItem" />
+        /// </param>
         /// <returns>通用回傳訊息格式</returns>
         [HttpPost]
         [JwtAuthFilter(AuthorizeBy.Admin | AuthorizeBy.UserSelf, RequirePrivilege.EditFlag)]
@@ -485,12 +474,10 @@ namespace NS_Education.Controller.Legacy.UserDataController
 
             // 只在輸入都驗證過後才允許更新 DB
             if (isValid)
-            {
                 await input
                     .StartValidate(true)
                     .ValidateAsync(async i => await UpdatePasswordForUserData(i.ID, i.NewPassword),
                         e => AddError(e.Message));
-            }
 
             // 2. 回傳通用訊息格式。
             return GetResponseJson();
@@ -499,7 +486,7 @@ namespace NS_Education.Controller.Legacy.UserDataController
         private async Task UpdatePasswordForUserData(int id, string inputPassword)
         {
             // 1. 查詢資料。無資料時拋錯
-            UserData queried = await DC.UserData.FirstOrDefaultAsync(ud => ud.UID == id);
+            var queried = await DC.UserData.FirstOrDefaultAsync(ud => ud.UID == id);
             if (queried == null)
                 throw new NullReferenceException(UserDataNotFound);
 
@@ -520,10 +507,10 @@ namespace NS_Education.Controller.Legacy.UserDataController
         #region GetList
 
         /// <summary>
-        /// 取得使用者列表。
+        ///     取得使用者列表。
         /// </summary>
-        /// <param name="input">輸入資料。請參照 <see cref="UserData_GetList_Input_APIItem"/>。</param>
-        /// <returns>回傳結果。請參照 <see cref="UserData_GetList_Output_APIItem"/>，以及 <see cref="UserData_GetList_Output_Row_APIItem"/>。</returns>
+        /// <param name="input">輸入資料。請參照 <see cref="UserData_GetList_Input_APIItem" />。</param>
+        /// <returns>回傳結果。請參照 <see cref="UserData_GetList_Output_APIItem" />，以及 <see cref="UserData_GetList_Output_Row_APIItem" />。</returns>
         [HttpGet]
         [JwtAuthFilter(AuthorizeBy.Admin, RequirePrivilege.ShowFlag)]
         public async Task<string> GetList(UserData_GetList_Input_APIItem input)
@@ -575,10 +562,12 @@ namespace NS_Education.Controller.Legacy.UserDataController
         #region GetInfoById
 
         /// <summary>
-        /// 查詢單筆使用者資料。
+        ///     查詢單筆使用者資料。
         /// </summary>
-        /// <param name="input">輸入資訊。請參照 <see cref="UserData_GetInfoById_Input_APIItem"/>。</param>
-        /// <returns><see cref="UserData_GetInfoById_Output_APIItem"/></returns>
+        /// <param name="id">UID</param>
+        /// <returns>
+        ///     <see cref="UserData_GetInfoById_Output_APIItem" />
+        /// </returns>
         [HttpGet]
         [JwtAuthFilter(AuthorizeBy.Admin | AuthorizeBy.UserSelf, RequirePrivilege.ShowFlag, "ID", null)]
         public async Task<string> GetInfoById(int id)
@@ -613,6 +602,5 @@ namespace NS_Education.Controller.Legacy.UserDataController
         }
 
         #endregion
-        
     }
 }

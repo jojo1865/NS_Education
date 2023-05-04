bf6d8348ad8b699c837a47bea4883063c1e3e373
KevinHuang <xiangwei.326@gmail.com>
2023-05-04 17:24:46 +0800

    Add: 針對 CORS preflight request 的處理

--- NS_Education/Global.asax.cs
+++ NS_Education/Global.asax.cs
@@ -1,9 +1,11 @@
+using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using Microsoft.IdentityModel.Logging;
+using NS_Education.Tools.Extensions;
 
 namespace NS_Education
 {
@@ -21,6 +23,33 @@ namespace NS_Education
                 // 預設行為會在 Log 不顯示機敏資料 (PII)。
                 IdentityModelEventSource.ShowPII = true; 
             #endif
+        }
+
+        protected void Application_BeginRequest()
+        {
+            // 處理 CORS Preflight Request
+            string originHeader = "";
+
+            if (Request.HttpMethod != "OPTIONS")
+                return;
+            
+            if (Request.Headers.AllKeys.Contains("Origin"))
+            {
+                originHeader = Request.Headers["Origin"];
+            }
+            else if (Request.Headers.AllKeys.Contains("Referer"))
+            {
+                originHeader = Request.Headers["Referer"];
+            }
+
+            if (!string.IsNullOrWhiteSpace(originHeader))
+            {
+                Response.Headers.Add("Access-Control-Allow-Origin", originHeader);
+            }
+            
+            if (!originHeader.IsNullOrWhiteSpace()) {
+                Response.Flush();
+            }
         }
     }
 }

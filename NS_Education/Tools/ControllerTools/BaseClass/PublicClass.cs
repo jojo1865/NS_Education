3c6218a6b2e4c2919e4647adbe24a8403989fd55
KevinHuang <xiangwei.326@gmail.com>
2023-05-09 09:21:38 +0800

    Change: 調整 GroupData.DeleteItem 行為，不再支援復活

--- NS_Education/Tools/ControllerTools/BaseClass/PublicClass.cs
+++ NS_Education/Tools/ControllerTools/BaseClass/PublicClass.cs
@@ -68,6 +68,16 @@ namespace NS_Education.Tools.ControllerTools.BaseClass
         }
 
         /// <summary>
+        /// 回傳一串不支援輸入值時可用的預設錯誤字串。
+        /// </summary>
+        /// <param name="fieldName">欄位名稱</param>
+        /// <returns>預設錯誤訊息字串</returns>
+        protected string UnsupportedValue(string fieldName)
+        {
+            return $"{fieldName} 不支援這個輸入值，請重新確認。";
+        }
+        
+        /// <summary>
         /// 回傳一串更新 DB 失敗時可用的預設錯誤字串。
         /// </summary>
         /// <param name="fieldName">欄位名稱</param>

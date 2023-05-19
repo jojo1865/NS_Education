96fb68376205b9591511f143577e9762b21cd02c
KevinHuang <xiangwei.326@gmail.com>
2023-05-19 15:50:36 +0800

    Refactor: 重構 BeingValidatedEnumerable 為類似 Wrapper 的設計，避免重複 Code

--- NS_Education/Tools/BeingValidated/BeingValidatedEnumerable.cs
+++ NS_Education/Tools/BeingValidated/BeingValidatedEnumerable.cs
@@ -7,15 +7,13 @@ namespace NS_Education.Tools.BeingValidated
     public class BeingValidatedEnumerable<TElement, TEnumerable> : IBeingValidated<TElement, TEnumerable>
         where TEnumerable : IEnumerable<TElement>
     {
-        private readonly TEnumerable _collection;
-        private bool _isInvalid;
-        private bool _skipIfAlreadyInvalid;
-        private bool IsLazyAndInvalid => _skipIfAlreadyInvalid && _isInvalid;
-        
+        private readonly TEnumerable _targetEnumerable;
+        private readonly IBeingValidated<TEnumerable, TEnumerable> _inner;
+
         public BeingValidatedEnumerable(TEnumerable target, bool skipIfAlreadyInvalid = false)
         {
-            _collection = target;
-            _skipIfAlreadyInvalid = skipIfAlreadyInvalid;
+            _targetEnumerable = target;
+            _inner = target.StartValidate(skipIfAlreadyInvalid);
         }
         
         /// <summary>
@@ -28,37 +26,14 @@ namespace NS_Education.Tools.BeingValidated
         public IBeingValidated<TElement, TEnumerable> Validate(Func<TElement, bool> validation,
             Action<TElement> onFail = null, Action<TElement, Exception> onException = null)
         {
-            foreach (TElement element in _collection)
+            foreach (TElement element in _targetEnumerable)
             {
-                try
-                {
-                    if (IsLazyAndInvalid) return this;
-                    if (validation.Invoke(element)) continue;
-                    
-                    DoWhenFailWithElement(onFail, element);
-                }
-                catch (Exception e)
-                {
-                    DoWhenExceptionWithElement(onException, e, element);
-                }
+                _inner.Validate(_ => validation.Invoke(element), 
+                    _ => onFail?.Invoke(element),
+                    (_, e) => onException?.Invoke(element, e));
             }
 
             return this;
-        }
-
-        private void DoWhenFailWithElement(Action<TElement> onFail, TElement element)
-        {
-            _isInvalid = true;
-            onFail?.Invoke(element);
-        }
-        
-        private void DoWhenExceptionWithElement(Action<TElement, Exception> onException, Exception e, TElement element)
-        {
-            _isInvalid = true;
-            if (onException is null)
-                throw e;
-            
-            onException.Invoke(element, e);
         }
 
         /// <summary>
@@ -69,17 +44,10 @@ namespace NS_Education.Tools.BeingValidated
         /// <returns>此物件本身</returns>
         public IBeingValidated<TElement, TEnumerable> Validate(Action<TElement> validation, Action<TElement, Exception> onException = null)
         {
-            foreach (TElement element in _collection)
+            foreach (TElement element in _targetEnumerable)
             {
-                try
-                {
-                    if (IsLazyAndInvalid) return this;
-                    validation.Invoke(element);
-                }
-                catch (Exception e)
-                {
-                    DoWhenExceptionWithElement(onException, e, element);
-                }
+                _inner.Validate(_ => validation.Invoke(element),
+                    (_, e) => onException?.Invoke(element, e));
             }
 
             return this;
@@ -96,19 +64,11 @@ namespace NS_Education.Tools.BeingValidated
         public async Task<IBeingValidated<TElement, TEnumerable>> ValidateAsync(Func<TElement, Task<bool>> validation,
             Action<TElement> onFail = null, Action<TElement, Exception> onException = null)
         {
-            foreach (TElement element in _collection)
+            foreach (TElement element in _targetEnumerable)
             {
-                try
-                {
-                    if (IsLazyAndInvalid) return this;
-                    if (await validation.Invoke(element)) continue;
-
-                    DoWhenFailWithElement(onFail, element);
-                }
-                catch (Exception e)
-                {
-                    DoWhenExceptionWithElement(onException, e, element);
-                }
+                await _inner.ValidateAsync(async _ => await validation.Invoke(element),
+                    _ => onFail?.Invoke(element),
+                    (_, e) => onException?.Invoke(element, e));
             }
 
             return this;
@@ -123,17 +83,10 @@ namespace NS_Education.Tools.BeingValidated
         /// <remarks><b>此處的異步實作並不是所有元素同時執行，仍然會照順序執行。異步的 scope 在於整組驗證，而非單個元素。</b></remarks>
         public async Task<IBeingValidated<TElement, TEnumerable>> ValidateAsync(Func<TElement, Task> validation, Action<TElement, Exception> onException)
         {
-            foreach (TElement element in _collection)
+            foreach (TElement element in _targetEnumerable)
             {
-                try
-                {
-                    if (IsLazyAndInvalid) return this;
-                    await validation.Invoke(element);
-                }
-                catch (Exception e)
-                {
-                    DoWhenExceptionWithElement(onException, e, element);
-                }
+                await _inner.ValidateAsync(async _ => await validation.Invoke(element),
+                    (_, e) => onException?.Invoke(element, e));
             }
 
             return this;
@@ -141,12 +94,12 @@ namespace NS_Education.Tools.BeingValidated
 
         public bool IsValid()
         {
-            return !_isInvalid;
+            return _inner.IsValid();
         }
 
         public IBeingValidated<TElement, TEnumerable> SkipIfAlreadyInvalid(bool setTo = true)
         {
-            _skipIfAlreadyInvalid = setTo;
+            _inner.SkipIfAlreadyInvalid(setTo);
             return this;
         }
     }

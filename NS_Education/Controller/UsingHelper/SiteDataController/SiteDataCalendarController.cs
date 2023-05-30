f4614554b81b60ca22dab3bf86c66f02ecbc139a
KevinHuang <xiangwei.326@gmail.com>
2023-05-30 12:03:05 +0800

    Change: 依據需求調整，調整 SiteData.GetCalendarList 的輸入，並補強驗證

--- NS_Education/Controller/UsingHelper/SiteDataController/SiteDataCalendarController.cs
+++ NS_Education/Controller/UsingHelper/SiteDataController/SiteDataCalendarController.cs
@@ -21,8 +21,9 @@ namespace NS_Education.Controller.UsingHelper.SiteDataController
     /// 但因為目前開的 Route 為 SiteData，因此還是歸類在 SiteDataController，<br/>
     /// 但處理的是預約單的 Entity。
     /// </summary>
-    public class SiteDataCalendarController : PublicClass, 
-        IGetListPaged<Resver_Site, SiteData_GetListForCalendar_Input_APIItem, SiteData_GetListForCalendar_Output_Row_APIItem>
+    public class SiteDataCalendarController : PublicClass,
+        IGetListPaged<Resver_Site, SiteData_GetListForCalendar_Input_APIItem,
+            SiteData_GetListForCalendar_Output_Row_APIItem>
     {
         #region Initializaton
 
@@ -30,18 +31,20 @@ namespace NS_Education.Controller.UsingHelper.SiteDataController
 
         public SiteDataCalendarController()
         {
-            _getListPagedHelper = new GetListPagedHelper<SiteDataCalendarController, Resver_Site, SiteData_GetListForCalendar_Input_APIItem,
-                SiteData_GetListForCalendar_Output_Row_APIItem>(this);
+            _getListPagedHelper =
+                new GetListPagedHelper<SiteDataCalendarController, Resver_Site,
+                    SiteData_GetListForCalendar_Input_APIItem,
+                    SiteData_GetListForCalendar_Output_Row_APIItem>(this);
         }
 
         #endregion
-        
+
         #region GetList - For calendar
-        
+
         // Route 為 /SiteData/GetCalendarList
         // 詳見 RouteConfig
         [HttpGet]
-        [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.ShowFlag, null ,null)]
+        [JwtAuthFilter(AuthorizeBy.Any, RequirePrivilege.ShowFlag, null, null)]
         public async Task<string> GetList(SiteData_GetListForCalendar_Input_APIItem input)
         {
             return await _getListPagedHelper.GetPagedList(input);
@@ -49,11 +52,19 @@ namespace NS_Education.Controller.UsingHelper.SiteDataController
 
         public async Task<bool> GetListPagedValidateInput(SiteData_GetListForCalendar_Input_APIItem input)
         {
-            bool isValid = input.StartValidate()
+            bool isValid = await input.StartValidate()
                 .Validate(i => i.TargetYear.IsInBetween(1911, 9999), () => AddError(WrongFormat("目標年分")))
                 .Validate(i => i.TargetMonth.IsInBetween(1, 12), () => AddError(WrongFormat("目標月份")))
                 .Validate(i => i.BSID.IsZeroOrAbove(), () => AddError(WrongFormat("欲篩選之場地 ID")))
                 .Validate(i => i.CID.IsZeroOrAbove(), () => AddError(WrongFormat("欲篩選之客戶 ID")))
+                .Validate(i => i.RHID.IsZeroOrAbove(), () => AddError(WrongFormat("欲篩選之預約單 ID")))
+                .SkipIfAlreadyInvalid()
+                .ValidateAsync(async i => await DC.B_SiteData.ValidateIdExists(i.BSID, nameof(B_SiteData.BSID)),
+                    () => AddError(NotFound("欲篩選之場地 ID")))
+                .ValidateAsync(async i => await DC.Customer.ValidateIdExists(i.CID, nameof(Customer.CID)),
+                    () => AddError(NotFound("欲篩選之客戶 ID")))
+                .ValidateAsync(async i => await DC.Resver_Head.ValidateIdExists(i.RHID, nameof(Resver_Head.RHID)),
+                    () => AddError(NotFound("欲篩選之預約單 ID")))
                 .IsValid();
 
             return await Task.FromResult(isValid);
@@ -67,15 +78,19 @@ namespace NS_Education.Controller.UsingHelper.SiteDataController
                 .Include(rs => rs.Resver_Head.M_Resver_TimeSpan.Select(rts => rts.D_TimeSpan))
                 .Include(rs => rs.B_SiteData)
                 .AsQueryable();
-            
+
             // 年份和月份
             query = query.Where(
                 rs => rs.TargetDate.Year == input.TargetYear && rs.TargetDate.Month == input.TargetMonth);
-            
+
+            // 預約單號
+            if (input.RHID.IsAboveZero())
+                query = query.Where(rs => rs.RHID == input.RHID);
+
             // 場地 ID
             if (input.BSID.IsAboveZero())
                 query = query.Where(rs => rs.BSID == input.BSID);
-            
+
             // 客戶 ID
             if (input.CID.IsAboveZero())
                 query = query.Where(rs => rs.Resver_Head.CID == input.CID);
@@ -101,17 +116,18 @@ namespace NS_Education.Controller.UsingHelper.SiteDataController
                     CID = entity.Resver_Head?.CID ?? 0,
                     CustomerTitle = entity.Resver_Head?.CustomerTitle ?? "",
                     TargetDate = entity.TargetDate.ToFormattedStringDate(),
-                    Items = entity.Resver_Head?.M_Resver_TimeSpan.Select(rts => new SiteData_GetListForCalendar_TimeSpan_APIItem
-                    {
-                        DTSID = rts.DTSID,
-                        Title = rts.D_TimeSpan.Title ?? "",
-                        SortNo = rts.SortNo
-                    }).OrderBy(item => item.SortNo)
+                    Items = entity.Resver_Head?.M_Resver_TimeSpan.Select(rts =>
+                            new SiteData_GetListForCalendar_TimeSpan_APIItem
+                            {
+                                DTSID = rts.DTSID,
+                                Title = rts.D_TimeSpan.Title ?? "",
+                                SortNo = rts.SortNo
+                            }).OrderBy(item => item.SortNo)
                         .ThenBy(item => item.DTSID)
                 }
             );
         }
-        
+
         #endregion
     }
 }
--- NS_Education/Models/APIItems/Controller/SiteData/GetListForCalendar/SiteData_GetListForCalendar_Input_APIItem.cs
+++ NS_Education/Models/APIItems/Controller/SiteData/GetListForCalendar/SiteData_GetListForCalendar_Input_APIItem.cs
@@ -6,6 +6,7 @@ namespace NS_Education.Models.APIItems.Controller.SiteData.GetListForCalendar
     {
         public int TargetYear { get; set; } = DateTime.Now.Year;
         public int TargetMonth { get; set; } = DateTime.Now.Month;
+        public int RHID { get; set; }
         public int BSID { get; set; }
         public int CID { get; set; }
     }

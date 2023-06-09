9625b9442d8b61f2c5e765fe56ee40a304921452
KevinHuang <xiangwei.326@gmail.com>
2023-06-09 11:03:44 +0800

    Fix: 修正 GetPagedList 正序 right 算法

--- NS_Education/Tools/Extensions/BaseRequestForPagedListExtension.cs
+++ NS_Education/Tools/Extensions/BaseRequestForPagedListExtension.cs
@@ -21,11 +21,11 @@ namespace NS_Education.Tools.Extensions
             {
                 // 正序時，照內建算式取值
                 left = input.GetStartIndex();
-                right = left + input.GetTakeRowCount();
+                right = left + input.GetTakeRowCount() - 1;
             }
             else
             {
-                // 正序時，從後方算回來，取得 right
+                // 倒序時，從後方算回來，取得 right
                 // 統一轉成 0-index 計算
                 right = totalRows - 1 - input.GetStartIndex();
                 left = Math.Max(0, right - (input.GetTakeRowCount() - 1));

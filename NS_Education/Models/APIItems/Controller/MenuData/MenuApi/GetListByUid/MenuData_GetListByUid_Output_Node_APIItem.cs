440dd10ba73512dff2780254e0bbc96ad5e9795d
KevinHuang <xiangwei.326@gmail.com>
2023-06-09 16:59:26 +0800

    Fix: 隱藏不需要的欄位

--- NS_Education/Models/APIItems/Controller/MenuData/MenuApi/GetListByUid/MenuData_GetListByUid_Output_Node_APIItem.cs
+++ NS_Education/Models/APIItems/Controller/MenuData/MenuApi/GetListByUid/MenuData_GetListByUid_Output_Node_APIItem.cs
@@ -13,7 +13,8 @@ namespace NS_Education.Models.APIItems.Controller.MenuData.MenuApi.GetListByUid
         public string Title { get; set; }
         public string URL { get; set; }
         public int SortNo { get; set; }
-        public int Index { get; private set; }
+
+        [JsonIgnore] public int Index { get; private set; }
 
         public IList<MenuData_GetListByUid_Output_Node_APIItem> Items { get; set; } =
             new List<MenuData_GetListByUid_Output_Node_APIItem>();

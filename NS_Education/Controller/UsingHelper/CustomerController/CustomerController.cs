6c849896c105aa6c8891c1983981507c5baf9696
KevinHuang <xiangwei.326@gmail.com>
2023-06-15 14:37:19 +0800

    Change: 調整 Customer.GetList 的排序方式

--- NS_Education/Controller/UsingHelper/CustomerController/CustomerController.cs
+++ NS_Education/Controller/UsingHelper/CustomerController/CustomerController.cs
@@ -174,9 +174,7 @@ namespace NS_Education.Controller.UsingHelper.CustomerController
                     c.Resver_Head.Any(rh => !rh.DeleteFlag && rh.B_StaticCode.Code != ReserveHeadState.Draft) ==
                     (input.ResverType == 1));
 
-            return query.OrderBy(c => c.Code.Length)
-                .ThenBy(c => c.Code)
-                .ThenBy(c => c.CID);
+            return query.OrderBy(c => c.CID);
         }
 
         public async Task<Customer_GetList_Output_Row_APIItem> GetListPagedEntityToRow(Customer entity,

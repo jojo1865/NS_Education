be2d0d8635dfa2ec3f01bf09f0a175fb6c562de3
KevinHuang <xiangwei.326@gmail.com>
2023-06-16 17:23:14 +0800

    Refactor: 整合重覆的實作

--- NS_Education/Models/APIItems/BaseInfusable.cs
+++ NS_Education/Models/APIItems/BaseInfusable.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace NS_Education.Models.APIItems
+﻿namespace NS_Education.Models.APIItems
 {
-    public abstract class BaseInfusable : IReturnMessageInfusable
+    public abstract class BaseInfusable : BaseApiResponse, IReturnMessageInfusable
     {
-        public bool SuccessFlag { get; private set; }
-        public IEnumerable<string> Messages { get; private set; } = new List<string>();
         public void Infuse(BaseApiResponse msg)
         {
             SuccessFlag = msg.SuccessFlag;

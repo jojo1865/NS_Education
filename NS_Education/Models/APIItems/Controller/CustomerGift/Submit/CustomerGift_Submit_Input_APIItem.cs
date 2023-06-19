411072f7f6308ebca6f596ce32b0ee1e0095d8f8
KevinHuang <xiangwei.326@gmail.com>
2023-06-19 17:46:36 +0800

    Fix: 修正錯誤的輸入欄位屬性

--- NS_Education/Models/APIItems/Controller/CustomerGift/Submit/CustomerGift_Submit_Input_APIItem.cs
+++ NS_Education/Models/APIItems/Controller/CustomerGift/Submit/CustomerGift_Submit_Input_APIItem.cs
@@ -4,7 +4,7 @@ namespace NS_Education.Models.APIItems.Controller.CustomerGift.Submit
 {
     public class CustomerGift_Submit_Input_APIItem : BaseRequestForSubmit
     {
-        public ICollection<CustomerGift_Submit_Customers_Row_APIItem> Customers =
+        public ICollection<CustomerGift_Submit_Customers_Row_APIItem> Customers { get; set; } =
             new List<CustomerGift_Submit_Customers_Row_APIItem>();
 
         public int GSID { get; set; }

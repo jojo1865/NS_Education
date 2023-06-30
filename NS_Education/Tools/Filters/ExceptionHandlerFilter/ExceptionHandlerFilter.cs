9776bf251739ac6bd14912a98cb3d33a51a5f0db
KevinHuang <xiangwei.326@gmail.com>
2023-06-30 11:20:38 +0800

    Add: 發生錯誤時，嘗試將錯誤記錄到 DB

--- NS_Education/Tools/Filters/ExceptionHandlerFilter/ExceptionHandlerFilter.cs
+++ NS_Education/Tools/Filters/ExceptionHandlerFilter/ExceptionHandlerFilter.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Web.Mvc;
+using NS_Education.Models.Entities;
+using NS_Education.Tools.Extensions;
 
 namespace NS_Education.Tools.Filters.ExceptionHandlerFilter
 {
@@ -7,12 +9,43 @@ namespace NS_Education.Tools.Filters.ExceptionHandlerFilter
     {
         public void OnException(ExceptionContext filterContext)
         {
+            try
+            {
+                WriteToDb(filterContext);
+            }
+            catch
+            {
+                // ignored
+                // Because there can be exception about connection with DB.
+            }
+
             filterContext.Result = ResponseHelper.CreateWrappedResponse(filterContext, filterContext.Result);
 
             Console.WriteLine(filterContext.Exception.Message);
             Console.WriteLine(filterContext.Exception.StackTrace);
 
             filterContext.ExceptionHandled = true;
+        }
+
+        private static void WriteToDb(ExceptionContext filterContext)
+        {
+            using (NsDbContext context = new NsDbContext())
+            {
+                // here we don't use project-customized SaveChanges method,
+                // to avoid possibilities where that fails too.
+
+                context.ErrorLog.Add(new ErrorLog
+                {
+                    JWT = FilterStaticTools.GetJwtToken(filterContext.HttpContext.Request),
+                    RequestUrl = filterContext.HttpContext.Request.RawUrl,
+                    Payload = Uri.UnescapeDataString(filterContext.HttpContext.Request.Form.ToString()),
+                    ExceptionMessage = filterContext.Exception.GetActualMessage(),
+                    ExceptionTrace = filterContext.Exception.GetMeaningfulStackTrace(),
+                    CreDate = DateTime.Now
+                });
+
+                context.SaveChanges();
+            }
         }
     }
 }

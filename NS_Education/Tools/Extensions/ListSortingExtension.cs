74da0a986471751b752ea22217be53b7c6f92ccb
KevinHuang <xiangwei.326@gmail.com>
2023-08-01 14:22:00 +0800

    Fix: 修正尋找 Property 時的設定

--- NS_Education/Tools/Extensions/ListSortingExtension.cs
+++ NS_Education/Tools/Extensions/ListSortingExtension.cs
@@ -12,10 +12,13 @@ namespace NS_Education.Tools.Extensions
         {
             ICollection<string> errors = new List<string>();
 
+            // 這邊 flags 的目標是忽略大小寫
+            // 但單純只放 IgnoreCase 會覆寫掉預設的 flags, 所以手動把預設的 flags 也加進來以便順利找到 property
+
             bool isValid = sorts.StartValidateElements()
                 .Validate(
-                    sort => sort.PropertyName.HasContent() &&
-                            typeof(TEntity).GetProperty(sort.PropertyName, BindingFlags.IgnoreCase) != null,
+                    sort => sort.PropertyName.HasContent() && typeof(TEntity).GetProperty(sort.PropertyName,
+                        BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance) != null,
                     sort => errors.Add($"套用排序時，查無此欄位：{sort.PropertyName}"))
                 .IsValid();
 

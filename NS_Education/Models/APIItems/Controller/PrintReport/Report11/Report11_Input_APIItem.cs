b790f6c4f166bf0b124f4c8b8dd530f3864f6769
KevinHuang <xiangwei.326@gmail.com>
2023-10-17 15:11:24 +0800

    Add: 報表11增加輸入欄位

--- NS_Education/Models/APIItems/Controller/PrintReport/Report11/Report11_Input_APIItem.cs
+++ NS_Education/Models/APIItems/Controller/PrintReport/Report11/Report11_Input_APIItem.cs
@@ -7,5 +7,11 @@ namespace NS_Education.Models.APIItems.Controller.PrintReport.Report11
     {
         public string StartDate { get; set; }
         public string EndDate { get; set; }
+
+        public string SiteName { get; set; }
+        public int? BCID { get; set; }
+        public bool? IsActive { get; set; }
+        public int? BSCID1 { get; set; }
+        public int? BasicSize { get; set; }
     }
 }
--- NS_Education/Models/APIItems/Controller/PrintReport/Report11/Report11_Output_APIItem.cs
+++ NS_Education/Models/APIItems/Controller/PrintReport/Report11/Report11_Output_APIItem.cs
@@ -20,12 +20,23 @@ namespace NS_Education.Models.APIItems.Controller.PrintReport.Report11
         public string StartDate { get; set; }
         public string EndDate { get; set; }
 
+        public string SiteName { get; set; }
+        public int? BCID { get; set; }
+        public bool? IsActive { get; set; }
+        public int? BSCID1 { get; set; }
+        public int? BasicSize { get; set; }
+
         public override void SetByInput(BaseRequestForPagedList input)
         {
             if (input is Report11_Input_APIItem r11)
             {
                 StartDate = r11.StartDate;
                 EndDate = r11.EndDate;
+                SiteName = r11.SiteName;
+                BCID = r11.BCID;
+                IsActive = r11.IsActive;
+                BSCID1 = r11.BSCID1;
+                BasicSize = r11.BasicSize;
             }
 
             base.SetByInput(input);

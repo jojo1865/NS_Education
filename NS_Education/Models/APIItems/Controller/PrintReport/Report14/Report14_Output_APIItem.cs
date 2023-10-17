839119c4f546fd7d3072ecf4ad3469e545a2575e
KevinHuang <xiangwei.326@gmail.com>
2023-10-17 15:19:42 +0800

    Fix: 報表14增加輸入欄位

--- NS_Education/Models/APIItems/Controller/PrintReport/Report14/Report14_Output_APIItem.cs
+++ NS_Education/Models/APIItems/Controller/PrintReport/Report14/Report14_Output_APIItem.cs
@@ -20,12 +20,23 @@ namespace NS_Education.Models.APIItems.Controller.PrintReport.Report14
         public string StartDate { get; set; }
         public string EndDate { get; set; }
 
+        public string SiteName { get; set; }
+        public int? BCID { get; set; }
+        public bool? IsActive { get; set; }
+        public int? BSCID1 { get; set; }
+        public int? BasicSize { get; set; }
+
         public override void SetByInput(BaseRequestForPagedList input)
         {
             if (input is Report14_Input_APIItem r14)
             {
                 StartDate = r14.StartDate?.ParseDateTime().ToString("yyyy/MM/dd");
                 EndDate = r14.EndDate?.ParseDateTime().ToString("yyyy/MM/dd");
+                SiteName = r14.SiteName;
+                BCID = r14.BCID;
+                IsActive = r14.IsActive;
+                BSCID1 = r14.BSCID1;
+                BasicSize = r14.BasicSize;
             }
 
             base.SetByInput(input);

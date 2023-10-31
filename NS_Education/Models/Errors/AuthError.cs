681a7a84ea8ef5debaed5c53e1ee4fae5d4cc5e5
KevinHuang <xiangwei.326@gmail.com>
2023-10-31 13:23:39 +0800

    Change: 讓 JWT 驗證失敗時的錯誤訊息更明瞭

--- NS_Education/Models/Errors/AuthError.cs
+++ NS_Education/Models/Errors/AuthError.cs
@@ -19,6 +19,6 @@ namespace NS_Education.Models.Errors
 
         public override char ErrorType => 'S';
         public override int ErrorCodeInt => 2;
-        public override string ErrorMessage => "登入已過期，或因系統異常而驗證失敗！請先嘗試重新登入，若問題未解決，聯絡系統管理員。";
+        public override string ErrorMessage => "您的帳號無權限進行此操作，或是登入已過期！請先嘗試重新登入，若問題未解決，請與系統管理員聯絡。";
     }
 }

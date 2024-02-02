39ce61fa33f770c3f794fe38586be1e035280e8d
teamolHuang <xiangwei.326@gmail.com>
2024-02-02 09:44:18 +0800

    Fix: [報表8] 補 Commit 檔案

---
+++ NS_Education/Models/APIItems/Controller/PrintReport/Report8/Report8_Output_Excel_Row_APIItem.cs
@@ -0,0 +1,19 @@
+namespace NS_Education.Models.APIItems.Controller.PrintReport.Report8
+{
+    public class Report8_Output_Excel_Row_APIItem
+    {
+        public string EventDate { get; set; }
+        public string CustomerTitle { get; set; }
+        public int A1 { get; set; }
+        public int A2 { get; set; }
+        public int A3 { get; set; }
+        public int B1 { get; set; }
+        public int B2 { get; set; }
+        public int C1 { get; set; }
+        public int C2 { get; set; }
+        public bool D1 { get; set; }
+        public string MK { get; set; }
+        public string OP { get; set; }
+        public int Month { get; set; }
+    }
+}

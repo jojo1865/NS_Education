37d99f5e1e39e9e03bc881d68a97d5250023131a
teamolHuang <xiangwei.326@gmail.com>
2024-08-28 15:55:39 +0800

    Fix: 修正場地類別編輯後資料會不見的現象

--- NS_Education/Models/APIItems/Controller/Category/Submit/Category_Submit_Input_APIItem.cs
+++ NS_Education/Models/APIItems/Controller/Category/Submit/Category_Submit_Input_APIItem.cs
@@ -5,6 +5,16 @@ namespace NS_Education.Models.APIItems.Controller.Category.Submit
         public int BCID { get; set; }
         public int ParentID { get; set; }
         public int CategoryType { get; set; }
+
+        /// <summary>
+        /// 前端相容別名欄位。前端有時候會丟這個欄位
+        /// </summary>
+        public int iCategoryType
+        {
+            get => CategoryType;
+            set => CategoryType = value;
+        }
+
         public string Code { get; set; }
         public string TitleC { get; set; }
         public string TitleE { get; set; }
